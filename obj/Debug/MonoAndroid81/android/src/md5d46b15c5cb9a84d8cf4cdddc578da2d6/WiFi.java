fa6a62c03a17a7686ba38d9ce314cae0a5b62b4b
mariuszdelikat <37081812+mariuszdelikat@users.noreply.github.com>
2018-11-04 12:00:32 +0100

    Add files via upload

---
+++ obj/Debug/MonoAndroid81/android/src/md5d46b15c5cb9a84d8cf4cdddc578da2d6/WiFi.java
@@ -0,0 +1,47 @@
+package md5d46b15c5cb9a84d8cf4cdddc578da2d6;
+
+
+public class WiFi
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("WIFINFO.WiFi, WIFINFO", WiFi.class, __md_methods);
+	}
+
+
+	public WiFi ()
+	{
+		super ();
+		if (getClass () == WiFi.class)
+			mono.android.TypeManager.Activate ("WIFINFO.WiFi, WIFINFO", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
